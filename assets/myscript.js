4c3cc36175469ce5177eac6b5283fd0a44f149fa
Lisonallie <allisonvanlinden@gmail.com>
2019-05-28 17:00:10 +0200

    add bg img to canvas element

--- assets/myscript.js
+++ assets/myscript.js
@@ -1,29 +1,43 @@
-window.onload = function () {
-    document.getElementById('cat').style.display = 'none';
-};
+var canvas = document.getElementById("canvas");
+canvas.width = canvas.innerWidth;
+canvas.height = canvas.innerHeight;
+var context = canvas.getContext("2d");
 
 
-document.getElementById("Nyan").addEventListener('click', hideShow);
-function hideShow() {
-    document.getElementById("cat").style.display = "block";
+function draw() {
+    var background = new Image();
+    background.src = "nyanbg.jpg";
+background.onload = function () {
+    ctx.drawImage(0, 0, canvas.width, canvas.height);
 }
 
 
-document.getElementById("Nyan").addEventListener('click', nyanRain);
-function nyanRain() {
-    var elem = document.getElementById("cat");
-    var pos = -100;
-    var id = setInterval(frame, 10);
-    function frame() {
-        if (pos == 640) {
-            clearInterval(id);
-        } else {
-            pos++;
-            elem.style.top = pos + 'px';
-            elem.style.left = pos + 'px';
-        }
-    }
-}
-function hideShow() {
-    document.getElementById("cat").style.display = "block";
-}
+// window.onload = function () {
+//     document.getElementById('cat').style.display = 'none';
+// };
+
+
+// document.getElementById("Nyan").addEventListener('click', hideShow);
+// function hideShow() {
+//     document.getElementById("cat").style.display = "block";
+// }
+
+
+// document.getElementById("Nyan").addEventListener('click', nyanRain);
+// function nyanRain() {
+//     var elem = document.getElementById("cat");
+//     var pos = -100;
+//     var id = setInterval(frame, 10);
+//     function frame() {
+//         if (pos == 640) {
+//             clearInterval(id);
+//         } else {
+//             pos++;
+//             elem.style.top = pos + 'px';
+//             elem.style.left = pos + 'px';
+//         }
+//     }
+// }
+// function hideShow() {
+//     document.getElementById("cat").style.display = "block";
+// }
